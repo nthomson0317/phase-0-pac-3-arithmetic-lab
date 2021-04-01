19e950e26f3f490dc33dd0ba8af2878b29db25b0
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-04-01 14:52:41 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(x, y){
+return (x + y);
+}
+
+function subtract(x, y) {
+    return (x - y);
+}
+
+function multiply(x, y) {
+    return (x * y);
+}
+
+function divide(x, y) {
+    return (x / y);
+}
+
+function increment(n) {
+    (n++);
+    return (n);
+    
+}
+
+function decrement(n) {
+    (n--);
+    return (n);
+}
+
+function makeInt(n){
+parseInt(n, 10);
+return (parseInt(n, 10));
+}
+
+function preserveDecimal(n) {
+parseFloat(n, 10);
+return (parseFloat(n,10));
+}
